573ff0a4b82ceb17027a006b1dc10ae6cbc7220f
DavidCosbyUofU <u1294088@umail.utah.edu>
2021-04-29 17:37:42 -0600

    documentation and refactoring for consistency

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,16 @@ mod room;
 mod room_controller;
 
 pub fn main() {
-    let w = Wall(Point(0.0, 0.0), Point(1.5, 0.0));
-    let w2 = Wall(Point(1.5, 0.0), Point(1.5, 1.5));
-    let w3 = Wall(Point(1.5, 1.5), Point(0.0, 1.5));
+    let w = Strip(Point(0.0, 0.0), Point(1.5, 0.0));
+    let w2 = Strip(Point(1.5, 0.0), Point(1.5, 1.5));
+    let w3 = Strip(Point(1.5, 1.5), Point(0.0, 1.5));
 
     let config = RoomConfig {
         led_density: 60.0,
         view_pos: Point(0.0, 0.0),
         view_rot: 90.0,
-        walls: vec![w, w2, w3],
-        pixels: vec![Color(0, 0, 0)],
+        strips: vec![w, w2, w3],
+        leds: vec![Color(0, 0, 0)],
     };
 
     println!("{}", config.num_leds());
--- src/room/mod.rs
+++ src/room/mod.rs
@@ -1,5 +1,6 @@
 #![crate_name = "doc"]
 
+/// Settings and data for a room
 pub struct RoomConfig {
     /// number of leds / meter
     pub led_density: f32,
@@ -7,24 +8,25 @@ pub struct RoomConfig {
     pub view_pos: Point,
     /// expected rotation of the observer in degrees
     pub view_rot: f32,
-    pub walls: Vec<Wall>,
-    pub pixels: Vec<Color>,
+    pub strips: Vec<Strip>,
+    pub leds: Vec<Color>,
 }
 
 impl RoomConfig {
-    /// returns the number of pixels in the light strip chain, derived from led_density.
+    /// returns the number of pixels in the light strip chain, derived from [`RoomConfig`].led_density
     pub fn num_leds(self) -> usize {
         let mut count = 0.0;
-        for w in self.walls {
+        for w in self.strips {
             count += w.len() * self.led_density;
         }
         return count as usize;
     }
 }
 
-pub struct Wall(pub Point, pub Point);
+/// LED light strip stretching from strip.0 to strip.1. Does not own any leds, see [`RoomConfig`].leds
+pub struct Strip(pub Point, pub Point);
 
-impl Wall {
+impl Strip {
     pub fn len(&self) -> f32 {
         let dx = self.1 .0 - self.0 .0;
         let dy = self.1 .1 - self.0 .1;
@@ -32,5 +34,8 @@ impl Wall {
     }
 }
 
+/// Barebones 2d tuple struct
 pub struct Point(pub f32, pub f32);
+
+///24-bit color tuple struct
 pub struct Color(pub u8, pub u8, pub u8);
