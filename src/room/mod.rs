ce1c115414296da921b60cddb82ed83e6cd45436
DavidCosbyUofU <u1294088@umail.utah.edu>
2021-04-29 21:28:49 -0600

    re-established types as tuple aliases

--- src/room/mod.rs
+++ src/room/mod.rs
@@ -23,19 +23,43 @@ impl RoomConfig {
     }
 }
 
-/// LED light strip stretching from strip.0 to strip.1. Does not own any leds, see [`RoomConfig`].leds
-pub struct Strip(pub Point, pub Point);
+pub type Point = (f32, f32);
+pub type Vector2D = Point;
 
-impl Strip {
-    pub fn len(&self) -> f32 {
+/// LED light strip stretching from strip.0 to strip.1. Does not own any leds, see [`RoomConfig`].leds
+pub type LineSegment = (Point, Point);
+pub type Strip = LineSegment;
+
+pub trait LineSegmentTrait {
+    fn len(&self) -> f32;
+    fn intersects(&self, other: &LineSegment) -> Option<Point>;
+}
+
+impl LineSegmentTrait for LineSegment {
+    fn len(&self) -> f32 {
         let dx = self.1 .0 - self.0 .0;
         let dy = self.1 .1 - self.0 .1;
         return (dx * dx + dy * dy).sqrt();
     }
+
+    fn intersects(&self, other: &LineSegment) -> Option<Point> {
+        let a1 = self.1 .1 - self.0 .1;
+        let b1 = self.0 .0 - self.1 .0;
+        let c1 = a1 * self.0 .0 + b1 * self.0 .1;
+
+        let a2 = other.1 .1 - other.0 .1;
+        let b2 = other.0 .0 - other.1 .0;
+        let c2 = a2 * other.0 .0 + b2 * other.0 .1;
+
+        let delta = a1 * b2 - a2 * b1;
+
+        if delta == 0.0 {
+            return None;
+        }
+
+        Some(((b2 * c1 - b1 * c2) / delta, (a1 * c2 - a2 * c1) / delta))
+    }
 }
 
-/// Barebones 2d tuple struct
-pub struct Point(pub f32, pub f32);
-
 ///24-bit color tuple struct
-pub struct Color(pub u8, pub u8, pub u8);
+pub type Color = (u8, u8, u8);
