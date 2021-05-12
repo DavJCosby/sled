e784f9ba7dd5c6ee505e67a6617f256035d8c368
DavidJCosby <davjcosby@gmail.com>
2021-05-12 16:27:22 -0600

    refactor to InputDevice

--- crates/slc/src/devices.rs
+++ crates/slc/src/devices.rs
@@ -3,7 +3,7 @@ use std::sync::{Arc, RwLock};
 use crate::room_controller::RoomController;
 
 /// Trait for anything that should send input to the [RoomController](../room_controller/struct.RoomController.html).
-pub trait InputDriver {
+pub trait InputDevice {
     /// Tells the driver to start sending input to the [RoomController](../room_controller/struct.RoomController.html).
     fn start(self, controller: Arc<RwLock<RoomController>>);
         /// Tells the driver to stop sending input to the [RoomController](../room_controller/struct.RoomController.html).
