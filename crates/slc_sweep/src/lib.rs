e09b87255edb4d817d07b3be9bd72e88894bcd7c
DavidCosbyUofU <70235806+DavidCosbyUofU@users.noreply.github.com>
2021-05-18 14:19:57 -0600

    simplified sweep

--- crates/slc_sweep/src/lib.rs
+++ crates/slc_sweep/src/lib.rs
@@ -1,4 +1,8 @@
-use std::{sync::RwLock, thread, time::Instant};
+use std::{
+    sync::RwLock,
+    thread,
+    time::{Duration, Instant},
+};
 
 use slc::devices::InputDevice;
 use slc::room_controller::RoomController;
@@ -18,33 +22,21 @@ impl InputDevice for Sweep {
         thread::spawn(move || {
             let start = Instant::now();
 
-            let controller_read = controller_copy.read().unwrap();
-            let leds = controller_read.room.leds();
-
             let mut last = 0.0;
 
-            while !self.stop == true {
+            while !self.stop {
                 let duration = start.elapsed().as_secs_f32();
-                if duration - last < 0.0025 {
+                if duration - last < 0.008333 {
+                    //thread::sleep(Duration::from_millis(1));
                     continue;
                 };
                 let x = duration.cos();
                 let y = duration.sin();
                 let mut controller_write = controller_copy.write().unwrap();
 
-                for index in 0..controller_write.room.leds().len() {
-                    let led = leds.get(index).unwrap();
-                    controller_write.set(
-                        index,
-                        (
-                            (led.0 as f32 * 0.999) as u8,
-                            (led.1 as f32 * 0.999) as u8,
-                            (led.2 as f32 * 0.999) as u8,
-                        ),
-                    )
-                }
-
+                controller_write.set_all((0, 0, 0));
                 controller_write.set_at_room_dir((x, y), (0, 255, 0));
+
                 drop(controller_write);
                 last = duration;
             }
