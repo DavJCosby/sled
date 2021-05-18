b1a695436416b224c1b796fbbe885c21346be703
DavidCosbyUofU <70235806+DavidCosbyUofU@users.noreply.github.com>
2021-05-18 14:26:08 -0600

    webserver playground updates

--- webserver_playground/src/main.rs
+++ webserver_playground/src/main.rs
@@ -1,10 +1,8 @@
 // CLIENT
 
 use slc::prelude::*;
-
-use slc_gui::Gui;
-use slc_lab_rainbow::*;
 use slc_net_client::Client;
+use slc_sweep::Sweep;
 
 pub fn main() {
     let room = Room::new_from_file("../room_configs/myroom.rcfg");
@@ -12,7 +10,7 @@ pub fn main() {
     let rc_input_handle = RoomController::new_thread_safe(room);
     let rc_output_handle = rc_input_handle.clone();
     // prepare input and output devices
-    let input = Rainbow::new();
+    let input = Sweep::new();
     let output = Client::new();
 
     input.start(rc_input_handle);
