65d3ecac5d05a66fc899db6ffc4c49637a20897e
DavidCosbyUofU <70235806+DavidCosbyUofU@users.noreply.github.com>
2021-05-20 15:17:27 -0600

    p#2

--- rpi/src/main.rs
+++ rpi/src/main.rs
@@ -43,7 +43,7 @@ impl OutputDevice for GPIOOutput {
             let leds = controller.leds_mut(0);
 
             let mut counter = 0;
-            for (r, b, g) in read.room.leds() {
+            for (b, g, r) in read.room.leds() {
                 leds[counter] = [*r, *g, *b, 0];
                 counter += 1;
             }
