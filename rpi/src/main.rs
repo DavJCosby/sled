46b19a41008a3d13f6c4b70854439329aa1a8aee
DavidJCosby <davjcosby@gmail.com>
2021-05-23 16:12:05 -0600

    split to second thread

--- rpi/src/main.rs
+++ rpi/src/main.rs
@@ -14,45 +14,47 @@ impl GPIOOutput {
 
 impl OutputDevice for GPIOOutput {
     fn start(&self, rc: std::sync::Arc<std::sync::RwLock<slc::prelude::RoomController>>) {
-        let read = rc.read().unwrap();
-        let num_leds = read.room.leds().len();
+        thread::spawn(move || {
+            let read = rc.read().unwrap();
+            let num_leds = read.room.leds().len();
 
-        println!("Booted room with {} leds.", num_leds);
-        let mut controller = ControllerBuilder::new()
-            .freq(800_000)
-            .dma(10)
-            .channel(
-                0,
-                ChannelBuilder::new()
-                    .pin(18)
-                    .count(num_leds as i32)
-                    .strip_type(StripType::Ws2811Rgb)
-                    .brightness(255)
-                    .build(),
-            )
-            .build()
-            .unwrap();
+            println!("Booted room with {} leds.", num_leds);
+            let mut controller = ControllerBuilder::new()
+                .freq(800_000)
+                .dma(10)
+                .channel(
+                    0,
+                    ChannelBuilder::new()
+                        .pin(18)
+                        .count(num_leds as i32)
+                        .strip_type(StripType::Ws2811Rgb)
+                        .brightness(255)
+                        .build(),
+                )
+                .build()
+                .unwrap();
 
-        let start = Instant::now();
-        let mut last = 0.0;
-        loop {
-            let duration = start.elapsed().as_secs_f32();
+            let start = Instant::now();
+            let mut last = 0.0;
+            loop {
+                let duration = start.elapsed().as_secs_f32();
 
-            if duration - last < REFRESH_TIMING {
-                continue;
+                if duration - last < REFRESH_TIMING {
+                    continue;
+                }
+
+                let leds = controller.leds_mut(0);
+
+                let mut counter = 0;
+                for (b, g, r) in read.room.leds() {
+                    leds[num_leds - counter - 1] = [*r, *g, *b, 0];
+                    counter += 1;
+                }
+
+                last = duration;
+                controller.render();
             }
-
-            let leds = controller.leds_mut(0);
-
-            let mut counter = 0;
-            for (b, g, r) in read.room.leds() {
-                leds[num_leds - counter - 1] = [*r, *g, *b, 0];
-                counter += 1;
-            }
-
-            last = duration;
-            controller.render();
-        }
+        });
     }
 }
 
