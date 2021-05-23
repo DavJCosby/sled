fa60b1ae770014a284f5815f3c431b75eb3874f8
DavidJCosby <davjcosby@gmail.com>
2021-05-23 16:26:04 -0600

    f

--- rpi/src/main.rs
+++ rpi/src/main.rs
@@ -28,7 +28,7 @@ impl OutputDevice for GPIOOutput {
                     ChannelBuilder::new()
                         .pin(18)
                         .count(num_leds as i32)
-                        .strip_type(StripType::Ws2811Rgb)
+                        .strip_type(StripType::Ws2811Bgr)
                         .brightness(255)
                         .build(),
                 )
