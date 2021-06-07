88b33b8ff4c577c8efc84ce1ef3dedb88813c84e
DavidJCosby <davjcosby@gmail.com>
2021-06-07 00:42:54 -0600

    a

--- final_setup/pi_server/src/gpio_output.rs
+++ final_setup/pi_server/src/gpio_output.rs
@@ -14,46 +14,48 @@ impl GPIOOutput {
 
 impl OutputDevice for GPIOOutput {
     fn start(&self, output_handle: RoomControllerOutputHandle) {
-        let read = output_handle.read().unwrap();
-        let num_leds = read.room_data.leds().len() as i32;
-        let brightness = read.room_data.brightness;
-        println!("booted room with {} leds.", num_leds);
-        drop(read);
-        let mut gpio_controller = ControllerBuilder::new()
-            .freq(800_000)
-            .dma(10)
-            .channel(
-                0,
-                ChannelBuilder::new()
-                    .pin(18)
-                    .count(num_leds)
-                    .strip_type(StripType::Ws2811Gbr) // Ws2811Grb
-                    .brightness(brightness)
-                    .build(),
-            )
-            .build()
-            .unwrap();
-
-        let start = Instant::now();
-        let mut last = 0.0;
-        loop {
-            let duration = start.elapsed().as_secs_f32();
-            if duration - last < REFRESH_TIMING {
-                continue;
-            }
-
+        thread::spawn(move || {
             let read = output_handle.read().unwrap();
-            gpio_controller.set_brightness(0, read.room_data.brightness);
+            let num_leds = read.room_data.leds().len() as i32;
+            let brightness = read.room_data.brightness;
+            println!("booted room with {} leds.", num_leds);
+            drop(read);
+            let mut gpio_controller = ControllerBuilder::new()
+                .freq(800_000)
+                .dma(10)
+                .channel(
+                    0,
+                    ChannelBuilder::new()
+                        .pin(18)
+                        .count(num_leds)
+                        .strip_type(StripType::Ws2811Gbr) // Ws2811Grb
+                        .brightness(brightness)
+                        .build(),
+                )
+                .build()
+                .unwrap();
 
-            let leds = gpio_controller.leds_mut(0);
-            let mut counter = 0;
-            for (r, g, b) in read.room_data.leds() {
-                leds[counter] = [*r, *g, *b, 0];
-                counter += 1;
+            let start = Instant::now();
+            let mut last = 0.0;
+            loop {
+                let duration = start.elapsed().as_secs_f32();
+                if duration - last < REFRESH_TIMING {
+                    continue;
+                }
+
+                let read = output_handle.read().unwrap();
+                gpio_controller.set_brightness(0, read.room_data.brightness);
+
+                let leds = gpio_controller.leds_mut(0);
+                let mut counter = 0;
+                for (r, g, b) in read.room_data.leds() {
+                    leds[counter] = [*r, *g, *b, 0];
+                    counter += 1;
+                }
+
+                gpio_controller.render().unwrap();
+                last = duration;
             }
-
-            gpio_controller.render().unwrap();
-            last = duration;
-        }
+        });
     }
 }
