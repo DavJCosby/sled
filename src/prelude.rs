2c3da0473cb1d8f8a6b2f3ee76aa60742b0ac3f2
David Cosby <davjcosby@gmail.com>
2023-09-22 19:07:40 -0600

    Nuked everything and set up a simple workstation to start prototyping.

--- src/prelude.rs
+++
@@ -1,8 +0,0 @@
-#[doc(hidden)]
-pub use crate::{
-    devices::{InputDevice, OutputDevice, RoomControllerInputHandle, RoomControllerOutputHandle},
-    room::Room,
-    room_controller::RoomController,
-    room_data::RoomData,
-    util::*,
-};
