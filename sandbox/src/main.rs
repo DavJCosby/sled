20a2da3a3a8f4d44d517f0ae36f3340b7b15d2bb
David Cosby <davjcosby@gmail.com>
2023-10-16 17:21:54 -0600

    basic draw and read methods

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,7 +1,15 @@
-use sled::{SledError, Sled};
+use colored::Colorize;
+use palette::Srgb;
+use sled::{Sled, SledError};
 
 fn main() -> Result<(), SledError> {
-    let _sled = Sled::new("./cfg/config1.toml")?;
+    let mut sled = Sled::new("./cfg/config1.toml")?;
+    sled.set_all(Srgb::new(1.0, 0.0, 0.0));
+
+    let new_colors = sled.get_colors();
+    for color in new_colors {
+        print!("{}", "⬤ ".truecolor(color.0, color.1, color.2));
+    }
 
     Ok(())
 }
