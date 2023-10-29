fdb6283f24ccd309439681e2affbb6e2d9d8508b
David Cosby <davjcosby@gmail.com>
2023-10-28 19:29:06 -0600

    set up 2d viewport using bevy

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,28 +1,67 @@
-use colored::Colorize;
 use sled::{color::Rgb, Sled, SledError};
 
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+
+#[derive(Component)]
+struct LedIndex(usize);
+
 fn main() -> Result<(), SledError> {
-    let mut sled = Sled::new("./cfg/config1.toml")?;
+    let sled = Sled::new("./cfg/config1.toml")?;
 
-    let max = sled.num_leds();
-
-    sled.for_each(|led| {
-        led.color = Rgb::new(led.index() as f32 / max as f32, 0.5, 0.5);
-    });
-
-    // sled.for_each_in_segment(1, |led, alpha| {
-    //     *led += Rgb::new(0.0, alpha, 0.0);
-    // })?;
-
-    // sled.for_each_in_segment(2, |led, alpha| {
-    //     *led += Rgb::new(0.0, 1.0 - alpha, 0.0);
-    // })?;
-
-    let new_colors = sled.read_colors();
-    for color in new_colors {
-        print!("{}", "⬤ ".truecolor(color.red, color.green, color.blue));
-    }
-    println!("");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .insert_resource(SledResource(sled))
+        .add_systems(Startup, setup)
+        .add_systems(Update, (update_colors, display_colors))
+        .run();
 
     Ok(())
 }
+
+fn update_colors(mut sled: ResMut<SledResource>, time: Res<Time>) {
+    let elapsed = time.elapsed_seconds_wrapped();
+    sled.0.for_each(|led| {
+        let pos = led.position();
+        led.color = Rgb::new((pos.x + elapsed).sin(), (pos.y + elapsed).sin(), 0.0);
+    });
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    sled: Res<SledResource>,
+) {
+    commands.spawn(Camera2dBundle::default());
+
+    for led in sled.0.read() {
+        let (r, g, b) = led.color.into_components();
+        let position = led.position() * 150.0 - Vec2::new(350.0, 200.0);
+        commands.spawn((
+            MaterialMesh2dBundle {
+                mesh: meshes.add(shape::Circle::new(3.).into()).into(),
+                material: materials.add(ColorMaterial::from(Color::rgb(r, g, b))),
+                transform: Transform::from_translation(Vec3::new(position.x, position.y, 0.)),
+                ..default()
+            },
+            LedIndex(led.index()),
+        ));
+    }
+    // Circle
+}
+
+fn display_colors(
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    sled: Res<SledResource>,
+    query: Query<(&mut Handle<ColorMaterial>, &LedIndex)>,
+) {
+    for (material, index) in query.iter() {
+        if let Some(material) = materials.get_mut(material) {
+            let (r, g, b) = sled.0.get(index.0).unwrap().color.into_components();
+            material.color = Color::rgb(r, g, b);
+        }
+    }
+}
+
+#[derive(Resource)]
+struct SledResource(Sled);
