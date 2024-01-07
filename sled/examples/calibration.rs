7a71cfdc96b2df0afe1f63d744fa04d81326433a
David Cosby <davjcosby@gmail.com>
2024-01-07 10:55:56 -0700

    Update calibration.rs

--- sled/examples/calibration.rs
+++ sled/examples/calibration.rs
@@ -1,13 +1,14 @@
 mod tui;
 
 use glam::Vec2;
-use tui::SledTerminalDisplay;
 use sled::{color::Rgb, Sled, SledError};
+use tui::SledTerminalDisplay;
 
 fn main() -> Result<(), SledError> {
     let mut sled = Sled::new("./examples/config.toml").unwrap();
     let mut display = SledTerminalDisplay::start("Sled Visualizer", sled.domain());
-    
+
+    sled.set_all(Rgb::new(0.1, 0.1, 0.1));
     sled.set_vertices(Rgb::new(0.75, 0.75, 0.75));
     sled.set_at_dir(Vec2::new(1.0, 0.0), Rgb::new(1.0, 0.0, 0.0))?;
     sled.set_at_dir(Vec2::new(-1.0, 0.0), Rgb::new(0.5, 0.0, 0.0))?;
