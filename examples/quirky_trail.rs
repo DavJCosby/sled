4b045794f5557f0e37dcdb0397cebd9149ab9997
David Cosby <davjcosby@gmail.com>
2024-02-09 17:01:43 -0700

    trim up dependencies, also oops wasn't supposed to be commented

--- examples/quirky_trail.rs
+++ examples/quirky_trail.rs
@@ -2,21 +2,21 @@ mod drivers;
 use drivers::quirky_trail;
 
 mod resources;
-// use resources::tui::SledTerminalDisplay;
+use resources::tui::SledTerminalDisplay;
 
 use sled::{scheduler::Scheduler, Sled};
 
 fn main() {
     let sled = Sled::new("./examples/resources/config.toml").unwrap();
-    // let mut display = SledTerminalDisplay::start("Quirky Trail", sled.domain());
+    let mut display = SledTerminalDisplay::start("Quirky Trail", sled.domain());
     let mut driver = quirky_trail::build_driver();
     driver.mount(sled);
 
     let mut scheduler = Scheduler::fixed_hz(500.0);
     scheduler.loop_until_err(|| {
         driver.step();
-        // display.leds = driver.read_colors_and_positions();
-        // display.refresh()?;
+        display.leds = driver.read_colors_and_positions();
+        display.refresh()?;
         Ok(())
     });
 }
