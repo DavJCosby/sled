117879aff820404741eca7754f8404038282c0a2
David Cosby <davjcosby@gmail.com>
2024-03-26 14:30:47 -0600

    gross fluff to keep things from getting compiled out

--- benches/ripples.rs
+++ benches/ripples.rs
@@ -13,14 +13,18 @@ fn ripples(c: &mut Criterion) {
     let simulated_hz = 144.0;
     let total_steps = (simulated_duration * simulated_hz) as usize;
     let timestep = Duration::from_secs_f32(1.0 / simulated_hz);
+    
+    let mut r = 0;
     c.bench_function("ripples", |b| {
         b.iter(|| {
             for _ in 0..total_steps {
                 driver.step_by(timestep);
                 let colors = driver.colors::<u8>();
+                r = colors[0].red;
             }
-        })
+        });
     });
+    println!("{}", r); // prevent compiler from optimizing away output steps
 }
 
 use criterion::{criterion_group, criterion_main, Criterion};
