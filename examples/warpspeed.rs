a6997567d5df30d728b532b912dded563d338826
David Cosby <davjcosby@gmail.com>
2024-04-04 22:14:19 -0600

    readme touchups

--- examples/warpspeed.rs
+++ examples/warpspeed.rs
@@ -4,13 +4,20 @@ use drivers::warpspeed;
 mod resources;
 use resources::tui::SledTerminalDisplay;
 
-use sled::{scheduler::Scheduler, Sled};
+use sled::{color::Rgb, scheduler::Scheduler, Sled};
 
 fn main() {
     let sled = Sled::new("./examples/resources/config.toml").unwrap();
     let mut display = SledTerminalDisplay::start("Warpspeed", sled.domain());
     let mut driver = warpspeed::build_driver();
     driver.mount(sled);
+
+    let mut vector: Vec<Rgb> = vec![];
+    vector.extend([
+        Rgb::new(1.0, 0.0, 0.0),
+        Rgb::new(0.0, 1.0, 0.0),
+        Rgb::new(0.0, 0.0, 1.0),
+    ]);
 
     //let mut scheduler = Scheduler::new(500.0);
     //scheduler.loop_until_err(|| {
